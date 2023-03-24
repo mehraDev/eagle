56b1580cece85d86a74721871c6a3a5bf1983a71
mehraDev <126305156+mehraDev@users.noreply.github.com>
2023-03-24 22:16:15 +0530

    feat: 🔥 Initialize Firebase in React app
    This commit adds Firebase to the React app by installing the necessary dependencies and initializing the Firebase SDK in the index.tsx file. The Firebase SDK was imported and initialized with the configuration information from the Firebase console. This will allow us to use Firebase services in our app such as authentication, real-time database, and cloud storage.

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzXT4FHB1kWd_jGkkLL1zdw-II7ryke3Y',
+  authDomain: 'cloudyug-f2fe4.firebaseapp.com',
+  projectId: 'cloudyug-f2fe4',
+  storageBucket: 'cloudyug-f2fe4.appspot.com',
+  messagingSenderId: '840976261947',
+  appId: '1:840976261947:web:22d5bcd65679b4110a4d1e',
+  measurementId: 'G-20HB7JBZMR',
+};
+
+export default firebaseConfig;
